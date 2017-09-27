27022dafbc6958b873c6a96359061db5fa0757f8
Pawan Kataria <pawankataria2.ece16@jecrc.ac.in>
2017-09-27 23:49:10 +0530

    GUI Practice

--- GUIPractice/FourthGUI.py
+++ GUIPractice/FourthGUI.py
@@ -5,4 +5,9 @@ import tkinter.messagebox
 root = tk.Tk()
 
 tk.messagebox.showinfo('Window Title', 'Text to display')
+answer = tkinter.messagebox.askquestion('Question', 'Do you like it!')
+if(answer == "yes"):
+    print("yes")
+else:
+    print("no")
 root.mainloop()
